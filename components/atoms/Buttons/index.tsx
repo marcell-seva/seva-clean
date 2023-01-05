cefbbf04027148b651734df54a4d215cfff6a180
arntonius <marcell.dermawan@seva.id>
2023-01-05 09:57:10 +0700

    add scss template

---
+++ components/atoms/Buttons/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+export default function Buttons() {
+  return <div className={styles.title}>Buttons</div>
+}
---
+++ components/molecules/Headers/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Headers() {
+  return <div>Headers</div>;
+}
