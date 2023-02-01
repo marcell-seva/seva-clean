97c096390f68b0c632b12532ae39c668d108f180
arntonius <marcell.dermawan@seva.id>
2023-02-01 23:08:43 +0700

    update slider

--- components/molecules/carList/index.tsx
+++ components/molecules/carList/index.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image'
 import styles from '../../../styles/CarList.module.css'
 import { Card, IconBackButton, IconNextButton } from '../../atoms'
 import { api } from '../../../services/api'
+import Script from 'next/script'
 import { useIsMobile } from '../../../utils'
 
 export default function CarList({ data }: any) {
@@ -46,6 +47,7 @@ export default function CarList({ data }: any) {
 
   return (
     <div className={styles.container}>
+      <Script src="/lazy.js" />
       <div className={styles.header}>
         <h1 className={styles.headerText}>Mobil Baru Terpopuler</h1>
         <a
@@ -81,27 +83,18 @@ export default function CarList({ data }: any) {
               </div>
             </>
           )}
-          <Swiper
-            modules={[Navigation, Lazy]}
-            lazy={true}
-            navigation={{
-              nextEl: '.image-swiper-button-next-car-list',
-              prevEl: '.image-swiper-button-prev-car-list',
-              disabledClass: 'swiper-button-disabled',
-            }}
-            slidesPerGroup={3}
-            slidesPerView={4}
-            spaceBetween={isMobile ? 140 : 240}
-          >
-            {carList.slice(0, 5).map((item: any, key: number) => (
-              <SwiperSlide key={key}>
-                <Card item={item} />
-              </SwiperSlide>
-            ))}
-            <SwiperSlide>
-              <ShadowSlide />
-            </SwiperSlide>
-          </Swiper>
+          <div className="swiper mySwiperProduct">
+            <div className="swiper-wrapper">
+              {carList.slice(0, 5).map((item: any, key: number) => (
+                <div className="swiper-slide" key={key}>
+                  <Card item={item} />
+                </div>
+              ))}
+              <div className="swiper-slide">
+                <ShadowSlide />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- public/lazy.js
+++ public/lazy.js
@@ -18,8 +18,43 @@ const swiperOptions = {
   },
 }
 
+const swiperOptionsProduct = {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.image-swiper-button-next-car-list',
+    prevEl: '.image-swiper-button-prev-car-list',
+  },
+  slidesPerGroup: 1,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  breakpoints: {
+    1024: {
+      slidesPerGroup: 3,
+      slidesPerView: 4,
+      spaceBetween: 240,
+    },
+    480: {
+      slidesPerGroup: 3,
+      slidesPerView: 4,
+      spaceBetween: 120,
+    },
+  },
+  on: {
+    // LazyLoad swiper images after swiper initialization
+    afterInit: (swiper) => {
+      new LazyLoad({
+        container: swiper.el,
+        cancel_on_exit: false,
+      })
+    },
+  },
+}
+
 // Initialize the first swiper right away
 const eagerSwiper = new Swiper('.mySwiper', swiperOptions)
+const eagerSwiperProduct = new Swiper('.mySwiperProduct', swiperOptionsProduct)
 
 new LazyLoad({
   elements_selector: '.swiper--lazy',
