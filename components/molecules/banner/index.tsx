0f652f7102d7a856f214a2c8bd0da3211a3421b3
Marcell Antonius Dermawan <marcell.dermawan@seva.id>
2023-02-15 14:09:20 +0700

    update logic datalayer

--- components/molecules/banner/index.tsx
+++ components/molecules/banner/index.tsx
@@ -6,23 +6,14 @@ import Widget from '../widget'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay, Navigation } from 'swiper'
 import LazyLoad from 'vanilla-lazyload'
+import amplitude from 'amplitude-js'
 export default function Banner({ data }: any) {
   const [isExpandForm, setIsExpandForm] = useState<boolean>(false)
+  const [index, setIndex] = useState<number>(0)
+  const [isActiveSending, setIsActiveSending] = useState<boolean>(true)
+  const [dataSentIndex, setDataSentIndex] = useState<Array<number>>([0])
   const [isUserOnBannerScreen, setIsUserOnBannerScreen] =
-    useState<boolean>(false)
-
-  const handleScroll = () => {
-    const position: number = window.pageYOffset
-    if (position > 470) setIsUserOnBannerScreen(false)
-    else setIsUserOnBannerScreen(true)
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true })
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
+    useState<boolean>(true)
 
   const pushDataLayerInit = (payload: any): void => {
     TagManager.dataLayer({
@@ -65,30 +56,85 @@ export default function Banner({ data }: any) {
       },
     })
   }
-  const swiperOptions = {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    autoplay: {
-      delay: 8000,
-    },
-    cssMode: true,
-    breakpoints: {
-      1024: {
-        cssMode: false,
-      },
-    },
-    on: {
-      // LazyLoad swiper images after swiper initialization
-      afterInit: (swiper: any) => {
-        new LazyLoad({
-          container: swiper.el,
-          cancel_on_exit: false,
-        })
-      },
-    },
+
+  const getTimeStamp = (): string | null => {
+    const timestamp: string | null = sessionStorage.getItem('timestamp')
+    return timestamp
   }
+
+  const setTimestamp = (): void => {
+    const currentTime = new Date().getTime()
+    sessionStorage.setItem('timestamp', currentTime.toString())
+  }
+
+  const isInBorderTime = (payload: number): boolean => {
+    const currentTime = new Date().getTime()
+    const constrainTime = 5 * 60 * 1000
+    const diffTime = currentTime - payload
+    return diffTime <= constrainTime
+  }
+
+  const isDataExist = (data: Array<number>, key: number) => {
+    const filtered = data.filter((item: number) => item === key)
+    return filtered.length > 0
+  }
+
+  const handleScroll = () => {
+    const position: number = window.pageYOffset
+    if (position > 470) {
+      setIsUserOnBannerScreen(false)
+    } else setIsUserOnBannerScreen(true)
+  }
+
+  useEffect(() => {
+    const timestamp: string | null = getTimeStamp()
+    if (timestamp === null) {
+      setTimestamp()
+      setIsActiveSending(true)
+      pushDataLayerInit(data[0])
+      sendAmplitudeBanner(data[0])
+    } else {
+      if (isInBorderTime(parseInt(timestamp))) {
+        setIsActiveSending(false)
+      } else {
+        setTimestamp()
+        setIsActiveSending(true)
+        pushDataLayerInit(data[0])
+        sendAmplitudeBanner(data[0])
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    eventListenerScroll()
+  }, [])
+
+  const eventListenerScroll = () => {
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }
+
+  const sendAmplitudeBanner = (payload: any): void => {
+    amplitude.getInstance().logEvent('WEB_TOP_BANNER_VIEW', {
+      Name: data[index].name,
+      Page_direction_URL: data[index].url,
+      Creative_context: data[index].creativeContext,
+    })
+  }
+  useEffect(() => {
+    if (isActiveSending && data.length > 0 && isUserOnBannerScreen) {
+      const isDataSlideExists = isDataExist(dataSentIndex, index)
+      if (!isDataSlideExists) {
+        const dataIndex = dataSentIndex
+        dataIndex.push(index)
+        setDataSentIndex(dataIndex)
+        pushDataLayerInit(data[index])
+        sendAmplitudeBanner(data[index])
+      }
+    }
+  }, [isActiveSending, index, data])
 
   return (
     <div className={isExpandForm ? styles.containerActive : styles.container}>
@@ -103,7 +149,8 @@ export default function Banner({ data }: any) {
           pagination={{
             el: '.swiper-pagination',
           }}
-          autoplay={{ delay: 8000 }}
+          loop={true}
+          autoplay={{ delay: 3000 }}
           modules={[Autoplay]}
           breakpoints={{ 1024: { cssMode: false } }}
           on={{
@@ -158,9 +205,13 @@ export default function Banner({ data }: any) {
           pagination={{
             el: '.swiper-pagination',
           }}
-          autoplay={{ delay: 8000 }}
+          loop={true}
+          autoplay={{ delay: 4000 }}
           modules={[Autoplay]}
           breakpoints={{ 1024: { cssMode: false } }}
+          onRealIndexChange={(element: any) => {
+            setIndex(element.realIndex)
+          }}
           on={{
             afterInit: (swiper: any) => {
               new LazyLoad({
--- next.config.js
+++ next.config.js
@@ -30,6 +30,7 @@ const nextConfig = {
       },
     ],
   },
+  reactStrictMode: false,
   async headers() {
     return [
       {
