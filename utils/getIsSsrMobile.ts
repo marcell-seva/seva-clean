36ad5d85a7ae95b97018b36b1ea2c146d231e7e1
Erlangga <erlangga.dwi@ifabula.com>
2023-07-10 12:13:34 +0800

    update, ssr mobile, ssr styled component

--- utils/getIsSsrMobile.ts
+++ utils/getIsSsrMobile.ts
@@ -2,7 +2,7 @@ import MobileDetect from 'mobile-detect'
 import { GetServerSidePropsContext } from 'next'
 
 export const getIsSsrMobile = (context: GetServerSidePropsContext) => {
-  const md = new MobileDetect(context.req.headers['user-agent'] as string)
-
-  return Boolean(md.mobile())
+  return Boolean(
+    context.req.headers['user-agent']?.toLowerCase().includes('mobile'),
+  )
 }
