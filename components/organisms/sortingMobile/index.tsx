f8ee081d668f596d146530dfbd5cd20daabc33bf
ErlanggaIfabula <erlangga.dwi@ifabula.com>
2023-09-04 09:09:01 +0700

    fix

--- components/organisms/sortingMobile/index.tsx
+++ components/organisms/sortingMobile/index.tsx
@@ -12,7 +12,7 @@ import { CountlyEventNames } from 'helpers/countly/eventNames'
 
 interface SortingMobileProps extends Omit<BottomSheetProps, 'children'> {
   onClose: () => void
-  onPickClose: (value: any) => any
+  onPickClose: (value: any, label: string) => any
   sortOptionMultiKK?: boolean
   selectedSortMultiKK?: string
 }
@@ -29,10 +29,7 @@ const SortingMobile = ({
   const onChooseOption = (value: FormControlValue, label: FormControlValue) => {
     patchFunnelQuery({ sortBy: value as string })
     trackPLPSortClick(label as string)
-    trackEventCountly(CountlyEventNames.WEB_PLP_SORT_OPTION_CLICK, {
-      SORT_VALUE: label,
-    })
-    onPickClose(value)
+    onPickClose(value, label as string)
   }
   return (
     <BottomSheet
