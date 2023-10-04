2f6644cbe704e0fb21f9d8dfdff849ae87aa4fc5
rogasper <royangagas@gmail.com>
2023-10-04 09:37:14 +0700

    (+) component info, dummy condition

--- pages/adaSEVAdiOTO/updateleadsformCM/[[...slug]].tsx
+++ pages/adaSEVAdiOTO/updateleadsformCM/[[...slug]].tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { InformationSection } from 'components/organisms'
+import { InferGetServerSidePropsType } from 'next'
 
-const UpdateLeadsFormCM = () => {
-  return <div>UpdateLeadsFormCM</div>
+const UpdateLeadsFormCM = ({
+  message,
+  isValid,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (!isValid) {
+    return <div className="blank"></div>
+  }
+
+  return (
+    <div>
+      <InformationSection />
+    </div>
+  )
 }
 
 export default UpdateLeadsFormCM
+
+export async function getServerSideProps(context: any) {
+  const detailId = context.query.slug[0]
+  const TokenStatic = context.query.slug[1]
+  let valid = true
+
+  // TODO: Check Token
+  if (TokenStatic !== 'SEv4Uh4Y') {
+    valid = false
+  }
+  // TODO: getDetail ID
+
+  try {
+    return {
+      props: {
+        message: 'hello',
+        isValid: valid,
+      },
+    }
+  } catch (error) {
+    throw error
+  }
+}
--- pages/adaSEVAdiOTO/updateleadsformOTO/[[...slug]].tsx
+++ pages/adaSEVAdiOTO/updateleadsformOTO/[[...slug]].tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import Seo from 'components/atoms/seo'
+import { InformationSection } from 'components/organisms'
+import { InferGetServerSidePropsType } from 'next'
+import { defaultSeoImage } from 'utils/helpers/const'
 
-const UpdateLeadsFormOTO = () => {
-  return <div>UpdateLeadsFormOTO</div>
+const UpdateLeadsFormOTO = ({
+  message,
+  isValid,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (!isValid) {
+    return <div className="blank"></div>
+  }
+  return (
+    <div>
+      <Seo
+        title="SEVA - Beli Mobil Terbaru Dengan Cicilan Kredit Terbaik"
+        description="Beli mobil terbaru dari Toyota, Daihatsu, BMW dengan Instant Approval*. Proses Aman & Mudah✅ Terintegrasi dengan ACC & TAF✅ SEVA member of ASTRA"
+        image={defaultSeoImage}
+      />
+      <InformationSection />
+    </div>
+  )
 }
 
 export default UpdateLeadsFormOTO
+
+export async function getServerSideProps(context: any) {
+  const detailId = context.query.slug[0]
+  const TokenStatic = context.query.slug[1]
+  let valid = true
+
+  // TODO: Check Token
+  if (TokenStatic !== 'SEv4Uh4Y') {
+    valid = false
+  }
+  // TODO: getDetail ID
+
+  try {
+    return {
+      props: {
+        message: 'hello',
+        isValid: valid,
+      },
+    }
+  } catch (error) {
+    throw error
+  }
+}
