8c1b0c5ea5b21c182502d15befc2f888f21cabbe
ridwanmaelfa <ridwan.maelfa@ifabula.com>
2023-10-04 10:27:19 +0700

    fix-conflict

--- components/organisms/PdpMobile/index.tsx
+++ components/organisms/PdpMobile/index.tsx
@@ -360,9 +360,11 @@ export default function NewCarVariantList({
             dataCar?.PELUANG_KREDIT_BADGE === 'Red'
           ? 'Sulit disetujui'
           : 'Null',
-      TENOR_OPTION: dataCar?.PELUANG_KREDIT_BADGE
-        ? dataCar?.TENOR_OPTION + ' Tahun'
-        : 'Null',
+      TENOR_OPTION:
+        window.location.href.includes('kredit') &&
+        dataCar?.PELUANG_KREDIT_BADGE !== 'Null'
+          ? dataCar?.TENOR_OPTION + ' Tahun'
+          : 'Null',
       TENOR_RESULT:
         dataCar?.TENOR_RESULT && dataCar?.TENOR_RESULT === 'Green'
           ? 'Mudah disetujui'
@@ -372,7 +374,10 @@ export default function NewCarVariantList({
       KK_RESULT: 'Null',
       IA_RESULT: 'Null',
       TEMAN_SEVA_STATUS: temanSevaStatus,
-      INCOME_LOAN_CALCULATOR: filterStorage?.monthlyIncome,
+      INCOME_LOAN_CALCULATOR:
+        resultSelected?.loanRankResult !== 'Null'
+          ? `Rp${Currency(filterStorage?.monthlyIncome)}`
+          : 'Null',
       INCOME_KUALIFIKASI_KREDIT: 'Null',
       INCOME_CHANGE: 'Null',
       OCCUPATION: 'Null',
