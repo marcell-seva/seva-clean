3c25cceb3ce2a3c7b7aa06593bacc7a7a5a5eed7
ridwanmaelfa <ridwan.maelfa@ifabula.com>
2023-10-10 16:57:18 +0700

    revert-function-fetchAllCarModels

--- pages/kalkulator-kredit/[[...slug]].tsx
+++ pages/kalkulator-kredit/[[...slug]].tsx
@@ -450,17 +450,14 @@ export default function LoanCalculatorPage() {
       objData,
     )
   }
-
   const fetchAllCarModels = async () => {
-    if (forms?.city?.id) {
-      const params = new URLSearchParams()
-      params.append('cityId', forms?.city?.id as string)
-      params.append('city', forms?.city?.cityCode as string)
+    const params = new URLSearchParams()
+    params.append('cityId', defaultCity.id as string)
+    params.append('city', defaultCity.cityCode as string)
 
-      const response = await api.getRecommendation('', { params })
+    const response = await api.getRecommendation('', { params })
 
-      setAllModalCarList(response.carRecommendations)
-    }
+    setAllModalCarList(response.carRecommendations)
   }
 
   const fetchCarVariant = async () => {
