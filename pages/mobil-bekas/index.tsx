a65e7384238db3761e8e9d93d2690e2fc8fe87f9
rogasper <royangagas@gmail.com>
2023-10-10 09:33:38 +0700

    header, footer, PLP mobil bekas

--- pages/mobil-bekas/index.tsx
+++ pages/mobil-bekas/index.tsx
@@ -1,7 +1,240 @@
-import React from 'react'
+import { PLP } from 'components/organisms'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { getMinMaxPrice, getNewFunnelRecommendations } from 'services/newFunnel'
+import { CarRecommendationResponse, MinMaxPrice } from 'utils/types/context'
+import {
+  CityOtrOption,
+  FooterSEOAttributes,
+  MobileWebTopMenuType,
+  NavbarItemResponse,
+} from 'utils/types/utils'
+import PLPDesktop from 'components/organisms/PLPDesktop'
+import { getIsSsrMobile } from 'utils/getIsSsrMobile'
+import { api } from 'services/api'
+import Seo from 'components/atoms/seo'
+import { defaultSeoImage } from 'utils/helpers/const'
+import { useUtils } from 'services/context/utilsContext'
+import { MobileWebFooterMenuType } from 'utils/types/props'
+import styles from 'styles/pages/plp.module.scss'
+import { CarProvider } from 'services/context'
+import { generateBlurRecommendations } from 'utils/generateBlur'
+import { monthId } from 'utils/handler/date'
+import { getCarBrand } from 'utils/carModelUtils/carModelUtils'
+import { useMediaQuery } from 'react-responsive'
+import { useIsMobileSSr } from 'utils/hooks/useIsMobileSsr'
+import { useRouter } from 'next/router'
 
-const UsedCarResultPage = () => {
-  return <div>Test</div>
+const UsedCarResultPage = ({
+  meta,
+  dataMobileMenu,
+  dataFooter,
+  dataCities,
+  isSsrMobileLocal,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter()
+  const todayDate = new Date()
+  const carBrand = meta.carRecommendations.carRecommendations[0]?.brand
+  const metaTitle = `Harga OTR ${carBrand} - Harga OTR dengan Promo Cicilan bulan ${monthId(
+    todayDate.getMonth(),
+  )} | SEVA`
+  const metaDesc = `Beli mobil ${todayDate.getFullYear()} terbaru di SEVA. Beli mobil secara kredit dengan Instant Approval*.`
+  const metaBrandDesc = `Beli mobil ${carBrand} ${todayDate.getFullYear()} terbaru secara kredit dengan Instant Approval*. Cari tau spesifikasi, harga, promo, dan kredit di SEVA`
+  const descTag = router.query.brand ? metaBrandDesc : metaDesc
+  const [isMobile, setIsMobile] = useState(isSsrMobileLocal)
+  const isClientMobile = useMediaQuery({ query: '(max-width: 1024px)' })
+  const {
+    saveDesktopWebTopMenu,
+    saveMobileWebTopMenus,
+    saveMobileWebFooterMenus,
+    saveCities,
+  } = useUtils()
+
+  useEffect(() => {
+    saveMobileWebTopMenus(dataMobileMenu)
+    saveMobileWebFooterMenus(dataFooter)
+    saveCities(dataCities)
+  }, [])
+
+  useEffect(() => {
+    setIsMobile(isClientMobile)
+  }, [isClientMobile])
+  return (
+    <>
+      <Seo title={metaTitle} description={descTag} image={defaultSeoImage} />
+      <CarProvider
+        car={null}
+        carOfTheMonth={[]}
+        typeCar={null}
+        carModel={null}
+        carModelDetails={null}
+        carVariantDetails={null}
+        recommendation={meta.carRecommendations.carRecommendations}
+        recommendationToyota={[]}
+      >
+        <div className={styles.mobile}>
+          <PLP minmaxPrice={meta.MinMaxPrice} />
+        </div>
+      </CarProvider>
+    </>
+  )
 }
 
 export default UsedCarResultPage
+
+type PLPProps = {
+  title: string
+  description: string
+  footer: FooterSEOAttributes
+  MinMaxPrice: MinMaxPrice
+  carRecommendations: CarRecommendationResponse
+}
+
+const getBrand = (brand: string | string[] | undefined) => {
+  if (String(brand).toLowerCase() === 'toyota') {
+    return 'Toyota'
+  } else if (String(brand).toLowerCase() === 'daihatsu') {
+    return 'Daihatsu'
+  } else if (String(brand).toLowerCase() === 'bmw') {
+    return 'Bmw'
+  } else {
+    return ''
+  }
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  meta: PLPProps
+  dataDesktopMenu: NavbarItemResponse[]
+  dataMobileMenu: MobileWebTopMenuType[]
+  dataFooter: MobileWebFooterMenuType[]
+  dataCities: CityOtrOption[]
+  isSsrMobileLocal: boolean
+}> = async (ctx) => {
+  ctx.res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=59',
+  )
+  const metabrand = getBrand(ctx.query.brand)
+  const footerTagBaseApi =
+    'https://api.sslpots.com/api/footer-seos/?filters[location_page2][$eq]=CarSearchResult'
+  const meta = {
+    title: 'SEVA',
+    description: '',
+    footer: {
+      location_tag: '',
+      location_page2: '',
+      title_1: '',
+      Title_2: '',
+      Title_3: '',
+      content_1: '',
+      Content_2: '',
+      Content_3: '',
+      createdAt: '',
+      updatedAt: '',
+      publishedAt: '',
+    },
+    MinMaxPrice: { maxPriceValue: 0, minPriceValue: 0 },
+    carRecommendations: {
+      carRecommendations: [],
+      lowestCarPrice: 0,
+      highestCarPrice: 0,
+    },
+  }
+
+  const {
+    downPaymentAmount,
+    brand,
+    bodyType,
+    priceRangeGroup,
+    age,
+    tenure,
+    monthlyIncome,
+    sortBy,
+  } = ctx.query
+
+  try {
+    const [
+      fetchFooter,
+      menuDesktopRes,
+      menuMobileRes,
+      footerRes,
+      cityRes,
+    ]: any = await Promise.all([
+      axios.get(footerTagBaseApi + metabrand),
+      api.getMenu(),
+      api.getMobileHeaderMenu(),
+      api.getMobileFooterMenu(),
+      api.getCities(),
+    ])
+
+    const footerData = fetchFooter.data.data
+
+    if (!priceRangeGroup) {
+      const minmax = await getMinMaxPrice()
+      const minmaxPriceData = minmax
+      meta.MinMaxPrice = {
+        minPriceValue: minmaxPriceData.minPriceValue,
+        maxPriceValue: minmaxPriceData.maxPriceValue,
+      }
+    }
+
+    const queryParam: any = {
+      ...(downPaymentAmount && { downPaymentType: 'amount' }),
+      ...(downPaymentAmount && { downPaymentAmount }),
+      ...(brand && {
+        brand: String(brand)
+          ?.split(',')
+          .map((item) => getCarBrand(item)),
+      }),
+      ...(bodyType && { bodyType: String(bodyType)?.split(',') }),
+      ...(priceRangeGroup
+        ? { priceRangeGroup }
+        : {
+            priceRangeGroup: `${meta.MinMaxPrice.minPriceValue}-${meta.MinMaxPrice.maxPriceValue}`,
+          }),
+      ...(age && { age }),
+      ...(tenure && { tenure }),
+      ...(monthlyIncome && { monthlyIncome }),
+      ...(sortBy && { sortBy }),
+    }
+
+    const [funnel] = await Promise.all([
+      getNewFunnelRecommendations({ ...queryParam }),
+    ])
+
+    const recommendation = funnel
+
+    if (footerData && footerData.length > 0) {
+      meta.footer = footerData[0].attributes
+    }
+
+    if (recommendation) {
+      meta.carRecommendations = recommendation
+    }
+
+    return {
+      props: {
+        meta,
+        dataDesktopMenu: menuDesktopRes.data,
+        dataMobileMenu: menuMobileRes.data,
+        dataFooter: footerRes.data,
+        dataCities: cityRes,
+        isSsrMobile: getIsSsrMobile(ctx),
+        isSsrMobileLocal: getIsSsrMobile(ctx),
+      },
+    }
+  } catch (e) {
+    return {
+      props: {
+        meta,
+        dataDesktopMenu: [],
+        dataMobileMenu: [],
+        dataFooter: [],
+        dataCities: [],
+        isSsrMobile: getIsSsrMobile(ctx),
+        isSsrMobileLocal: getIsSsrMobile(ctx),
+      },
+    }
+  }
+}
