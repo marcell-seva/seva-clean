2d903dd9eadd6fc9ce266c0bf1cf75610c4f2df0
ErlanggaIfabula <erlangga.dwi@ifabula.com>
2023-10-12 13:42:03 +0700

    defer script update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,8 +38,13 @@ import { client } from 'utils/helpers/const'
 import { IsSsrMobileContext } from 'services/context/isSsrMobileContext'
 import { useAddUtmTagsToApiCall } from 'utils/hooks/useAddUtmTagsToApiCall/useAddUtmTagsToApiCall'
 import Head from 'next/head'
-import { CityFirst } from 'components/molecules/cityFirst'
 import { useAfterInteractive } from 'utils/hooks/useAfterInteractive'
+import dynamic from 'next/dynamic'
+
+const CityFirst = dynamic(
+  () => import('components/molecules/cityFirst').then((comp) => comp.CityFirst),
+  { ssr: true },
+)
 
 const kanyonLight = localFont({
   src: '../public/revamp/fonts/Kanyon/Kanyon-Light.otf',
@@ -114,19 +119,6 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <IsSsrMobileContext.Provider value={pageProps.isSsrMobile}>
         <GlobalContextProvider>
-          <Script
-            async
-            type="text/javascript"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                            !function(e,n,i,t,a,r,o,d){var s=e[a]=e[a]||[];if(s.invoked=0,s.initialised>0||s.invoked>0)return console.error("MoEngage Web SDK initialised multiple times. Please integrate the Web SDK only once!"),!1;e.moengage_object=a;var l={},g=function n(i){return function(){for(var n=arguments.length,t=Array(n),a=0;a<n;a++)t[a]=arguments[a];(e.moengage_q=e.moengage_q||[]).push({f:i,a:t})}},u=["track_event","add_user_attribute","add_first_name","add_last_name","add_email","add_mobile","add_user_name","add_gender","add_birthday","destroy_session","add_unique_user_id","moe_events","call_web_push","track","location_type_attribute"],m={onsite:["getData","registerCallback"]};for(var c in u)l[u[c]]=g(u[c]);for(var v in m)for(var f in m[v])null==l[v]&&(l[v]={}),l[v][m[v][f]]=g(v+"."+m[v][f]);r=n.createElement(i),o=n.getElementsByTagName("head")[0],r.async=1,r.src=t,o.appendChild(r),e.moe=e.moe||function(){return(s.invoked=s.invoked+1,s.invoked>1)?(console.error("MoEngage Web SDK initialised multiple times. Please integrate the Web SDK only once!"),!1):(d=arguments.length<=0?void 0:arguments[0],l)},r.addEventListener("load",function(){if(d)return e[a]=e.moe(d),e[a].initialised=e[a].initialised+1||1,!0}),r.addEventListener("error",function(){return console.error("Moengage Web SDK loading failed."),!1})}(window,document,"script","https://cdn.moengage.com/webpush/moe_webSdk.min.latest.js","Moengage");
-                            Moengage = moe({
-                            app_id:"KW8JVVD7VJKF2EQHOHX2YYOA",
-                            debug_logs: 0
-                            });`,
-            }}
-          ></Script>
           <style jsx global>{`
             :root {
               --kanyon-light: ${kanyonLight.style.fontFamily};
