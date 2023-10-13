c32869bb75af3919921769d378efe5f8f8829f09
toni612 <antonius.ifabula@ifabula.com>
2023-10-13 13:00:09 +0700

    hotfix : font weight and PDP tab filtering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -54,11 +54,13 @@ const kanyonMedium = localFont({
   src: '../public/revamp/fonts/Kanyon/Kanyon-Medium.otf',
   style: 'normal',
   display: 'swap',
+  weight: '600',
 })
 const kanyonBold = localFont({
   src: '../public/revamp/fonts/Kanyon/Kanyon-Bold.otf',
   style: 'normal',
   display: 'swap',
+  weight: '700',
 })
 const OpenSans = localFont({
   src: '../public/revamp/fonts/OpenSans/OpenSans-Regular.woff2',
@@ -69,11 +71,13 @@ const OpenSansSemiBold = localFont({
   src: '../public/revamp/fonts/OpenSans/OpenSans-SemiBold.woff2',
   style: 'normal',
   display: 'swap',
+  weight: '600',
 })
 const OpenSansBold = localFont({
   src: '../public/revamp/fonts/OpenSans/OpenSans-Bold.woff2',
   style: 'normal',
   display: 'swap',
+  weight: '700',
 })
 const OpenSansExtraBold = localFont({
   src: '../public/revamp/fonts/OpenSans/OpenSans-ExtraBold.woff2',
