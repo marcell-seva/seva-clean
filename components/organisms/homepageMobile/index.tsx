597da7bce662758059c350b85c34cc56c12f80e9
Marcell Antonius Dermawan <marcell.dermawan@seva.id>
2023-10-27 09:51:02 +0700

    hotfix meta desc on homepage mobile

--- components/organisms/homepageMobile/index.tsx
+++ components/organisms/homepageMobile/index.tsx
@@ -304,7 +304,7 @@ const HomepageMobile = ({ dataReccomendation, ssr }: any) => {
     <>
       <Seo
         title="SEVA - Beli Mobil Terbaru Dengan Cicilan Kredit Terbaik"
-        description="Beli mobil terbaru dari Toyota, Daihatsu, BMW dengan Instant Approval*. Proses Aman & Mudah✅ Terintegrasi dengan ACC & TAF✅ SEVA member of ASTRA"
+        description={`Beli mobil terbaru dari Toyota, Daihatsu, Isuzu, BMW, Peugeot ${new Date().getFullYear()} secara kredit dengan Instant Approval di SEVA. Proses Aman & Mudah, Terintegrasi dengan ACC & TAF. SEVA member of ASTRA`}
         image={defaultSeoImage}
       />
 
