247ee55ea1aaf1f5556ddff59cbb954b57e3b499
ErlanggaIfabula <erlangga.dwi@ifabula.com>
2023-11-24 15:36:21 +0700

    revert

--- pages/ktp/edit/index.tsx
+++ pages/ktp/edit/index.tsx
@@ -502,6 +502,10 @@ const KtpForm = () => {
         <div
           className={styles.back__button}
           onClick={() => {
+            saveSessionStorage(
+              SessionStorageKey.LastVisitedPageKKIAFlow,
+              window.location.pathname,
+            )
             router.back()
           }}
         >
